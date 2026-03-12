47ee18462697ee98e21706e26e12597ecd57abcb
devysonrobert <devysonrobert@users.noreply.github.com>
2026-03-12 14:42:31 +0000

    deploy: fix títulos XML — datas históricas NF-e aceitas como Válido

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1975057518"
+    serviceWorkerVersion: "2038342923"
   }
 });
